1a4c2de7102e1619f952890483802cd345a3fa0f
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-01-14 09:34:26 +0800

    Experimentation with Deserialization of Object
    Basic attempt to the deserialization of a serialized object written on a file. Written deserializedObject() method to deserialize object from file, cast the information from the file to an instance of same object, and print out one attributes from the casted object to check if method is successful

--- BeatBox/Serialization/Main.java
+++ BeatBox/Serialization/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         ObjectToSave berlinData = new ObjectToSave("John Berlin", "Moises Padilla", "Male");
         serializeObjectToSave(berlinData);
+        deserializeObject();
     }
 
     public static void serializeObjectToSave(Object objectReference) {
@@ -21,5 +22,26 @@ public class Main {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void deserializeObject() {
+        Object johnBerlin;
+        ObjectToSave johnBerlinData = new ObjectToSave(null, null, null);
+
+        try {
+            FileInputStream fileToDeserialize = new FileInputStream("John Berlin's Data.ser");
+            ObjectInputStream readDeserializedFile = new ObjectInputStream(fileToDeserialize);
+            try {
+                johnBerlin = readDeserializedFile.readObject();
+                johnBerlinData = (ObjectToSave) johnBerlin;
+                readDeserializedFile.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(johnBerlinData.name);
     }
 }
