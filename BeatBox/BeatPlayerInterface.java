d5c151b84337a201e76013e7841e5fcab98c376f
John Berlin Leonor <117552434+RuralNative@users.noreply.github.com>
2023-01-15 19:29:26 +0800

    Main and GUI Class Refactor
    Finished refactoring for Main class and the GUI class (BeatPlayerInterface); proceed to work on refactoring the MIDI class responsible for the actual work of the BeatPlayer

--- BeatBox/BeatPlayerInterface.java
+++ BeatBox/BeatPlayerInterface.java
@@ -8,8 +8,11 @@ import java.awt.event.*;
 
 public class BeatPlayerInterface {
 
-    JFrame mainFrame;
+    JFrame interfaceFrame;
     JPanel mainPanel;
+    JPanel instrumentCheckBoxPanel;
+    Box buttonBox;
+    Box instrumentNameBox;
     Sequencer musicSequencer;
     Sequence musicSequence;
     Track musicTrack;
@@ -17,85 +20,114 @@ public class BeatPlayerInterface {
     String[] instrumentList = {"Bass Drum", "Closed Hi-hat", "Open Hi-hat", "Acoustic Snare", "Crash Symbal", "Hand Clap", "High Tom", "High Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "L ow-mid Tom", "High Agogo", "Open Hi Conga"};
     int[] instrumentIndex = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
     
-    //Method to create GUI 
-    public void buildInterface() {
-        //Creates Frame of which all GUI components shall be built
-        mainFrame = new JFrame("Cyber BeatBox");
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    public void buildPlayerInterface() {
+        buildFrame("Cyber BeatBox");
+        buildPanel();
+        buildBoxWithButtons();
+        buildBoxForInstrumentNames();
+        insertBoxesToEastAndWestRegions();
+        buildInstrumentCheckBoxPanelonMainPanel();
+    }
+
+
+    private void buildFrame(String frameTitle) {
+        //Creates Frame of which all GUI components shall be built
+        interfaceFrame = new JFrame(frameTitle);
+        //Set Properties of Frame
+        interfaceFrame.setBounds(50, 50, 300, 300);
+        interfaceFrame.pack();
+        interfaceFrame.setVisible(true);
+        interfaceFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+    private void buildPanel() {
         //Creates Panel (backgroundPanel) with BorderLayOut and set Properties where components shall be built on
         BorderLayout backgroundLayout = new BorderLayout();
-        JPanel backgroundPanel = new JPanel(backgroundLayout);
-        backgroundPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        mainPanel = new JPanel(backgroundLayout);
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+    }
 
-        checkBoxList = new ArrayList<JCheckBox>();
 
-        //Creates Box to Add Buttons On
-        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+    private void buildBoxWithButtons() {
+        buttonBox = new Box(BoxLayout.Y_AXIS);
 
-        //Creates Buttons then Add to Box
         JButton startButton = new JButton("Start");
         startButton.addActionListener(new StartButtonListener());
         buttonBox.add(startButton);
-        //
+        
         JButton stopButton = new JButton("Stop");
         stopButton.addActionListener(new StopButtonListener());
         buttonBox.add(stopButton);
-        //
+        
         JButton increaseTempoButton = new JButton("Tempo Up");
         increaseTempoButton.addActionListener(new IncreaseTempoButtonListener());
         buttonBox.add(increaseTempoButton);
-        //
+        
         JButton decreaseTempoButton = new JButton("Tempo Down");
         decreaseTempoButton.addActionListener(new DecreaseTempoButtonListener());
         buttonBox.add(decreaseTempoButton);
+    }
 
-        //Creates Box to Put Instrument Names On
-        Box instrumentNameBox = new Box(BoxLayout.Y_AXIS);
-        for (int instrumentIndex = 0; instrumentIndex < 16; instrumentIndex++) {
-            instrumentNameBox.add(new Label (instrumentList[instrumentIndex]));
-        }
 
-        //Add the Two Boxes to Panel EAST and WEST regions
-        backgroundPanel.add(BorderLayout.EAST, buttonBox);
-        backgroundPanel.add(BorderLayout.WEST, instrumentNameBox);
+    private void buildBoxForInstrumentNames() {
+        instrumentNameBox = new Box(BoxLayout.Y_AXIS);
+            for (int instrumentIndex = 0; instrumentIndex < 16; instrumentIndex++) {
+                instrumentNameBox.add(new Label (instrumentList[instrumentIndex]));
+            }
+    }
 
-        //Add Panel (backgroundPanel) to Frame
-        mainFrame.getContentPane().add(backgroundPanel);
 
-        //Build Panel (mainPanel) on Panel (backgroundPanel)
+    private void insertBoxesToEastAndWestRegions() {
+        mainPanel.add(BorderLayout.EAST, buttonBox);
+        mainPanel.add(BorderLayout.WEST, instrumentNameBox);
+    }
+
+
+    private void buildInstrumentCheckBoxPanelonMainPanel() {
         GridLayout gridLayout = new GridLayout(16, 16);
         gridLayout.setVgap(1);
         gridLayout.setHgap(2);
         mainPanel = new JPanel(gridLayout);
-        backgroundPanel.add(BorderLayout.CENTER, mainPanel);
+        mainPanel.add(BorderLayout.CENTER, instrumentCheckBoxPanel);
 
-        //Build Checkboxes for Panel (mainPanel)
+        checkBoxList = new ArrayList<JCheckBox>();
         for (int loopCount = 0; loopCount < 256; loopCount++) {
             JCheckBox beatCheckBox = new JCheckBox();
             beatCheckBox.setSelected(false);
             checkBoxList.add(beatCheckBox);
-            mainPanel.add(beatCheckBox);
+            instrumentCheckBoxPanel.add(beatCheckBox);
         }
+    }
+    
 
-
-        //Set Up Midi Functions (NOT GUI)
-        setUpMidi();
-
-        //Set Properties of Frame
-        mainFrame.setBounds(50, 50, 300, 300);
-        mainFrame.pack();
-        mainFrame.setVisible(true);
+    //INNER CLASSES FOR BUTTON ACTION LISTENERS
+    public class StartButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            System.out.println("Start Button Clicked");
+            MusicPlayer player = new MusicPlayer();
+            player.buildTrackAndStart();
+        }
+    }
+    
+    public class StopButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            musicSequencer.stop();
+        }
+    }
+    
+    public class IncreaseTempoButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            float tempoFactor = musicSequencer.getTempoFactor();
+            musicSequencer.setTempoFactor((float)(tempoFactor * 1.03));
+        }
+    }
+   
+    public class DecreaseTempoButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            float tempoFactor = musicSequencer.getTempoFactor();
+            musicSequencer.setTempoFactor((float)(tempoFactor * 0.97));
+        }
     }
 }
-
-private void buildFrame(String frameTitle) {
-    //Creates Frame of which all GUI components shall be built
-    mainFrame = new JFrame(frameTitle);
-    //Set Properties of Frame
-    mainFrame.setBounds(50, 50, 300, 300);
-    mainFrame.pack();
-    mainFrame.setVisible(true);
-    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-}
-
